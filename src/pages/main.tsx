ef60faef21998b4b2ebaee38036fc7b2e21841e9
tunip9410 <tunip943@gmail.com>
2023-10-03 02:03:01 +0900

    backup

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components/native";
 import {useEffect, useState} from "react";
 import * as Location from "expo-location";
+import {Text, View} from "react-native";
 
 const MainLayer = styled.View`
     justify-content: center;
@@ -18,6 +19,7 @@ const MainContentLayer = styled.View`
 
 const SubContentLayer = styled.View`
     flex: 1;
+    width: 100%;
 `
 
 const TempText = styled.Text`
@@ -36,12 +38,15 @@ const CityName = styled(Description)`
     margin: 0;
 `
 
+const ForecastLayer = styled.View`
+    flex: 1;
+    border: 1px solid black;
+`
+
 function Main() {
-    const [city, setCity] = useState("")
-    const [description, setDescription] = useState("")
-    const [temp, setTemp] = useState(0)
-    const [tempMax, setTempMax] = useState(0)
-    const [tempMin, setTempMin] = useState(0)
+    const [weather, setWeather] = useState()
+    const [forecastArr, setForecastArr] = useState<object[]>([])
+    const API_KEY = "a1215692d81f82a8f734b4e483c2af15"
 
     async function getLocation() {
         await Location.requestForegroundPermissionsAsync();
@@ -50,31 +55,55 @@ function Main() {
         } = await Location.getCurrentPositionAsync();
         console.log(latitude, longitude)
 
-        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a1215692d81f82a8f734b4e483c2af15&units=metric`).then((res :any) => res.json()).then((json :any) => {
-            console.log(json)
-            setCity(json.name)
-            setDescription(json.weather[0].description)
-            setTemp(json.main.temp)
-            setTempMin(json.main.temp_min)
-            setTempMax(json.main.temp_max)
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`).then((res :any) => res.json()).then((json :any) => {
+            setWeather(json)
+        })
+    }
+
+    async function getForecast() {
+        await Location.requestForegroundPermissionsAsync();
+        const {coords: { latitude, longitude }} = await Location.getCurrentPositionAsync();
+
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`).then((res) => res.json()).then((json) => {
+            setForecastArr(json.list)
         })
     }
 
     useEffect(() => {
+        getForecast().then(r => r)
         getLocation().then(r => r)
     }, [])
 
-    return (
-        <MainLayer>
-            <MainContentLayer>
-                <TempText>{temp.toFixed(1)}</TempText>
-                <Description>{description}</Description>
-                <CityName>{city}</CityName>
-                <CityName>{tempMax.toFixed(1)} / {tempMin.toFixed(1)}</CityName>
-            </MainContentLayer>
-            <SubContentLayer></SubContentLayer>
-        </MainLayer>
-    )
+    if (forecastArr[0] !== undefined) {
+
+    }
+
+    if (weather !== undefined) {
+        console.log(weather.sys.sunrise, weather.sys.sunset)
+
+        return (
+            <MainLayer>
+                <MainContentLayer>
+                    <TempText>{weather.main.temp.toFixed(1)}</TempText>
+                    <Description>{weather.weather[0].description}</Description>
+                    <CityName>{weather.name}</CityName>
+                    <CityName>{weather.main.temp_max.toFixed(1)} / {weather.main.temp_min.toFixed(1)}</CityName>
+                </MainContentLayer>
+                <SubContentLayer>
+                    {forecastArr
+                        .slice(0, 20)
+                        .map((e, i :number) => (
+                        <View key={i}>
+                            <Text>{e.main.temp.toFixed(1)}</Text>
+                            <Text>{new Date(e.dt * 1000).toString()}</Text>
+                        </View>
+                    ))}
+                </SubContentLayer>
+            </MainLayer>
+        )
+    } else {
+        return null
+    }
 }
 
 export default Main
